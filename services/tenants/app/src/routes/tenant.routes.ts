fbd421f494cdc3d8275be898a0ff11818fe40822
arturfurtado <artururtado@gmail.com>
2026-04-12 15:27:59 -0300

    feat(tenants): add create tenant route and service WIP

--- services/tenants/app/src/routes/tenant.routes.ts
+++ services/tenants/app/src/routes/tenant.routes.ts
@@ -1,9 +1,17 @@
-import { app } from '../app'
+import Elysia, { t } from "elysia";
+import { CreateTenant } from "../services/create.tenant";
 
-app.get("/tenants", () => {
-    return "Tenants"
-})
+const createTenant = new CreateTenant()
 
-app.post("/tenants", () => {
-    return "Create Tenant"
-})
+export const tenants = new Elysia({prefix: "/tenants"}).post("/", ({body}) => {
+    const {name, description, cpfCnpj} = body
+    return createTenant.execute(name, description, cpfCnpj)
+}, 
+{
+    body: t.Object({
+        name: t.String(),
+        description: t.String(),
+        cpfCnpj: t.String() //todo: create a cpfCnpj class with a regex validator 
+    })
+}
+)
--- services/tenants/app/src/services/create.tenant.ts
+++ services/tenants/app/src/services/create.tenant.ts
@@ -1,3 +1,11 @@
-export class CreateTenant {
+import { db } from "../db/connection";
+import { tenants } from "../db/database.schema";
 
+export class CreateTenant {
+    public execute(name: string, description: string, cpfCnpj: string) {
+        const createTenant =  db.insert(tenants).values({name, description, cpfCnpj})
+    
+        return createTenant
 }
+}
+
